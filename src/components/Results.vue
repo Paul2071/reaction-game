bf051ff499669d9e7a04d9fbfd431471ba7db573
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2023-06-24 10:26:10 +0100

    added rank strings that are loaded via an if statement using the mounted hook

--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,6 +1,7 @@
 <template>
         <div>
     <p> Reaction time - {{propScore}}ms </p>
+    <p class="rank">{{ rank }} </p>
 </div>
 
 </template>
@@ -8,7 +9,31 @@
 <script>
 
     export default {
-        props: ["propScore"]
+        props: ["propScore"],
+        data() {
+            return {
+                rank: null
+            }
+        },
+        mounted() {
+            if (this.score < 250) {
+                this.rank = "fastest"
+            } else if (this.score < 400 ) {
+                this.rank = "fast"
+            } else {
+                this.rank = "kinda fast"
+            }
+        }
     }
 
 </script>
+
+<style>
+
+.rank {
+    font-size: 1.4em;
+    color: #444;
+    font-weight: bold;
+}
+
+</style>
